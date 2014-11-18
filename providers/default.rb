108a0f0bead9d964585c442be85893f8e8d185ff
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-11-18 15:38:03 -0300

    Fix nginx site

--- providers/default.rb
+++ providers/default.rb
@@ -116,41 +116,19 @@ def nginx_options_for(action, name, options)
     # path for static files
     "root"      => nginx_document_root(::File.join('current', options['relative_document_root'] || 'public')),
     "locations" => {
-      %q(/) => {
-        "proxy_set_header"       => ["X-Forwarded-For $proxy_add_x_forwarded_for",
+      "@#{nginx_upstream}" => {
+        "proxy_set_header"  => ["X-Forwarded-For $proxy_add_x_forwarded_for",
                                      "Host $http_host"],
-        # If the file exists as a static file serve it directly without
-        # running all the other rewite tests on it
-        "if" => {
-          "-f $request_filename" => {
-            "break" => nil
-          },
-          # check for index.html for directory index
-          # if its there on the filesystem then rewite
-          # the url to add /index.html to the end of it
-          # and then break to send it to the next config rules.
-          "-f $request_filename/index.html" => {
-            "rewrite" => "(.*) $1/index.html break"
-          },
-          # this is the meat of the rack page caching config
-          # it adds .html to the end of the url and then checks
-          # the filesystem for that file. If it exists, then we
-          # rewite the url to have explicit .html on the end
-          # and then send it on its way to the next config rule.
-          # if there is no file on the fs then it sets all the
-          # necessary headers and proxies to our upstream pumas
-          "-f $request_filename.html" => {
-            "rewrite" => "(.*) $1.html break"
-          },
-          "!-f $request_filename" => {
-            "proxy_pass" => "http://#{nginx_upstream}",
-            "break" => nil
-          }
-        }
+        "proxy_redirect" => "off",
+        "proxy_pass" => "http://#{nginx_upstream}",
+      },
+      %q(/) => {
+        "try_files" => "$uri @#{nginx_upstream}",
       }.merge(options['allow'] ? {'allow' => options['allow'], 'deny' => 'all'}: {}),
       # Now this supposedly should work as it gets the filenames with querystrings that Rails provides.
       # BUT there's a chance it could break the ajax calls.
       %q(~* \.(ico|css|gif|jpe?g|png|js)(\?[0-9]+)?$) => {
+        "try_files"     => "$uri @#{nginx_upstream}",
         "access_log"    => "off",
         "log_not_found" => "off",
         "expires"       => "max",
@@ -165,13 +143,13 @@ def nginx_options_for(action, name, options)
       # retrieve needed images/icons/frames, connections are cheap in
       # nginx so increasing this is generally safe...
       "keepalive_timeout" => "10",
-      "client_max_body_size" => "2G",
+      "client_max_body_size" => "50M",
       # this rewrites all the requests to the maintenance.html
       # page if it exists in the doc root. This is for capistrano's
       # disable web task
       "if" => {
-        "-f $document_root/maintenance.html" => {
-          "rewrite" =>  "^(.*)$  /maintenance.html last",
+        "-f $document_root/mantenimiento.html" => {
+          "rewrite" =>  "^(.*)$  /mantenimiento.html last",
           "break" => nil,
         }
      },
