99a7834349477bcb6855274e5d665ee765c6e0d7
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-04-16 13:26:49 -0300

    upgrade ruby version to 2.2.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
-default['mo_application_ruby']['rbenv']['ruby_version'] = "2.2.1"
+default['mo_application_ruby']['rbenv']['ruby_version'] = "2.2.2"
