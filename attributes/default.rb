9e19e8e91d45c72813b863da6dd0e308709f262f
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-04-22 11:09:22 -0300

    Fix backup gem ruby version

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,5 @@ default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
 default['mo_application_ruby']['rbenv']['ruby_version'] = "2.2.2"
+
+node.set['mo_backup']['ruby_version'] = node['mo_application_ruby']['rbenv']['ruby_version']
